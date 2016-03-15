c7b165d35a5c3d886b755036e55b6ac3453c191c
Sébastien Lesaint <sebastien.lesaint@sonarsource.com>
2016-03-15 12:36:18 +0100

    update 9010.sh to also change H2 DB port

--- 9010.sh
+++ 9010.sh
@@ -9,7 +9,8 @@ source scripts/property_utils.sh
 
 SQ_HOME=$1
 
-echo "WebServer on port 9010 and SearchServer on port 9011"
+echo "WebServer on port 9010, SearchServer on port 9011 and H2 on port 10002"
 set_property sonar.web.port 9010 $SQ_HOME/conf/sonar.properties
 set_property sonar.search.port 9011 $SQ_HOME/conf/sonar.properties
-
+set_property sonar.embeddedDatabase.port 10002 $SQ_HOME/conf/sonar.properties
+set_property sonar.jdbc.url jdbc:h2:tcp://localhost:10002/sonar $SQ_HOME/conf/sonar.properties
