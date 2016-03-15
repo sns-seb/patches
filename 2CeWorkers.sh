6b4cfeba20ae0b5a1779ce998e9ad06711457aee
Sébastien Lesaint <sebastien.lesaint@sonarsource.com>
2016-03-15 12:03:47 +0100

    add script to make CE run on two workers

---
+++ 2CeWorkers.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+###############################
+# sets property sonar.ce.workers to 2 in sonar.properties to process two CE tasks concurrently
+###############################
+
+set -euo pipefail
+
+source scripts/property_utils.sh
+
+SQ_HOME=$1
+
+echo "configuring CE to run 2 workers in parallel"
+set_property sonar.ce.workerCount 2 $SQ_HOME/conf/sonar.properties
