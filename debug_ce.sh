a577b98062b996e90c0e4181c4f75ce62f4092fe
Sébastien Lesaint <sebastien.lesaint@sonarsource.com>
2016-03-15 14:16:15 +0100

    add debug_ce.sh

---
+++ debug_ce.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+###############################
+# sets property sonar.ce.javaAdditionalOpts in sonar.properties to activate debug on port 5006
+###############################
+
+set -euo pipefail
+
+source scripts/property_utils.sh
+
+SQ_HOME=$1
+
+echo "enabling debug for Compute Engine, listening on port 5006"
+set_property sonar.ce.javaAdditionalOpts -agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=5006 $SQ_HOME/conf/sonar.properties
