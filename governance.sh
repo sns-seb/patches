f437a7708a885f90b0808aaac892cc5dcb15ef6e
Sébastien Lesaint <sebastien.lesaint@sonarsource.com>
2016-03-15 12:01:18 +0100

    update path since governance is now a maven multimodule project

--- governance.sh
+++ governance.sh
@@ -9,7 +9,7 @@ source scripts/property_utils.sh
 
 SQ_HOME=$1
 
-GOVERNANCE_FILE=~/DEV/governance/target/sonar-governance-plugin-1.0-SNAPSHOT.jar
+GOVERNANCE_FILE=~/DEV/governance/sonar-governance-plugin/target/sonar-governance-plugin-1.0-SNAPSHOT.jar
 EXT_DIR=$SQ_HOME/extensions/plugins/
 echo "copy $GOVERNANCE_FILE to $EXT_DIR"
 cp  $GOVERNANCE_FILE $EXT_DIR
