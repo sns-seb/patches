491957faf34a2e917e50aa2155aa795155802055
Sébastien Lesaint <sebastien.lesaint@sonarsource.com>
2016-03-15 14:16:03 +0100

    rename debug.sh to debug_web.sh

--- debug_web.sh
+++ debug_web.sh
@@ -9,5 +9,5 @@ source scripts/property_utils.sh
 
 SQ_HOME=$1
 
-echo "enabling debug in conf/sonar.properties, listening on port 5005"
+echo "enabling debug for Web Sever, listening on port 5005"
 set_property sonar.web.javaAdditionalOpts -agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=5005 $SQ_HOME/conf/sonar.properties
