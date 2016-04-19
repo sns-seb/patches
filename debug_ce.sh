f9f98a05b6d6f2c8a0c7f67e95c870d5a8a50a85
Sébastien Lesaint <sebastien.lesaint@sonarsource.com>
2016-04-19 12:06:35 +0200

    make debug of CE blocking

--- debug_ce.sh
+++ debug_ce.sh
@@ -10,4 +10,4 @@ source scripts/property_utils.sh
 SQ_HOME=$1
 
 echo "enabling debug for Compute Engine, listening on port 5006"
-set_property sonar.ce.javaAdditionalOpts -agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=5006 $SQ_HOME/conf/sonar.properties
+set_property sonar.ce.javaAdditionalOpts -agentlib:jdwp=transport=dt_socket,server=y,suspend=y,address=5006 $SQ_HOME/conf/sonar.properties
