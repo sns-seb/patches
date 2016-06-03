0f3ee2c22085580e20c05fc05a2a8d50e97c0a6a
Sébastien Lesaint <sebastien.lesaint@sonarsource.com>
2016-06-03 10:47:07 +0200

    remove sonar.jdbc.url, useless with SQ 5.6+

--- 9010.sh
+++ 9010.sh
@@ -13,4 +13,3 @@ echo "WebServer on port 9010, SearchServer on port 9011 and H2 on port 10002"
 set_property sonar.web.port 9010 $SQ_HOME/conf/sonar.properties
 set_property sonar.search.port 9011 $SQ_HOME/conf/sonar.properties
 set_property sonar.embeddedDatabase.port 10002 $SQ_HOME/conf/sonar.properties
-set_property sonar.jdbc.url jdbc:h2:tcp://localhost:10002/sonar $SQ_HOME/conf/sonar.properties
