e1d4bdd9248e966fdab8b71cf6b384d36248e1b0
Sébastien Lesaint <sebastien.lesaint@sonarsource.com>
2016-11-15 11:39:05 +0100

    debug_web: enable suspend

--- debug_web.sh
+++ debug_web.sh
@@ -10,4 +10,4 @@ source scripts/property_utils.sh
 SQ_HOME=$1
 
 echo "enabling debug for Web Sever, listening on port 5005"
-set_property sonar.web.javaAdditionalOpts -agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=5005 $SQ_HOME/conf/sonar.properties
+set_property sonar.web.javaAdditionalOpts -agentlib:jdwp=transport=dt_socket,server=y,suspend=y,address=5005 $SQ_HOME/conf/sonar.properties
