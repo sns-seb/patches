118ec722828a1976334e87f466b7845c2d1ff250
Sébastien Lesaint <sebastien.lesaint@sonarsource.com>
2016-11-15 11:40:27 +0100

    add "sonar.web.dev.source" to dev.sh

--- dev.sh
+++ dev.sh
@@ -11,3 +11,4 @@ SQ_HOME=$1
 
 echo "enabling dev mode"
 set_property sonar.web.dev true $SQ_HOME/conf/sonar.properties
+#set_property sonar.web.dev.source "/home/sebastienl/DEV/sonarqube/server/sonar-web/src/main/webapp" $SQ_HOME/conf/sonar.properties
