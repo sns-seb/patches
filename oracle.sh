37e89fdefec83062e1bc971cfefa73e356c0ba47
Sébastien Lesaint <sebastien.lesaint@sonarsource.com>
2016-11-15 11:34:48 +0100

    support adding jdbc driver to SQ in mssql and oracle patches

--- oracle.sh
+++ oracle.sh
@@ -9,6 +9,16 @@ source scripts/property_utils.sh
 
 SQ_HOME=$1
 PROPERTY_FILE=$SQ_HOME/conf/sonar.properties
+ORACLE_DRIVER_DIR=$SQ_HOME/extensions/jdbc-driver/oracle
+DRIVER_FILENAME=ojdbc6-11.2.0.3.0.jar
+SRC_DIR=/home/sebastienl/DEV/patches
+
+if [ ! -e $ORACLE_DRIVER_DIR ]; then
+  mkdir -p $ORACLE_DRIVER_DIR
+fi
+if [ ! -e $ORACLE_DRIVER_DIR/$DRIVER_FILENAME ]; then
+  cp $SRC_DIR/$DRIVER_FILENAME $ORACLE_DRIVER_DIR/
+fi
 
 echo "using Oracle"
 set_property sonar.jdbc.username sonar $PROPERTY_FILE
