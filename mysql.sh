cc04cfada586ad1cd1ebe6364fbb2ba30dc9d6af
Sébastien Lesaint <sebastien.lesaint@sonarsource.com>
2017-09-26 11:15:38 +0200

    support latest VM configuration in mysql

--- mysql.sh
+++ mysql.sh
@@ -11,7 +11,7 @@ SQ_HOME=$1
 PROPERTY_FILE=$SQ_HOME/conf/sonar.properties
 
 echo "using MySql"
-set_property sonar.jdbc.username sonar $PROPERTY_FILE
-set_property sonar.jdbc.password sonar $PROPERTY_FILE
-set_property sonar.jdbc.url "jdbc:mysql://localhost:3306/sonar?useUnicode=true&characterEncoding=utf8&rewriteBatchedStatements=true" $PROPERTY_FILE
+set_property sonar.jdbc.username sonarqube $PROPERTY_FILE
+set_property sonar.jdbc.password sonarqube $PROPERTY_FILE
+set_property sonar.jdbc.url "jdbc:mysql://localhost:3306/sonarqube?useUnicode=true&characterEncoding=utf8&rewriteBatchedStatements=true" $PROPERTY_FILE
 
