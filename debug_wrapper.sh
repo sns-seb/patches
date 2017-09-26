427dd47fb2ea0e04ad85ee8f285beba44c40ac3d
Sébastien Lesaint <sebastien.lesaint@sonarsource.com>
2017-09-26 11:14:48 +0200

    fix debug_wrapper

--- debug_wrapper.sh
+++ debug_wrapper.sh
@@ -9,7 +9,8 @@ SQ_HOME=$1
 
 echo "enabling debug in conf/wrapper.conf, listening on port 6006 and blocking"
 
-echo "wrapper.java.additional.1=-Xdebug" >> $SQ_HOME/conf/wrapper.conf
-echo "wrapper.java.additional.2=-Xrunjdwp:transport=dt_socket,address=6006,server=y,suspend=y" >> $SQ_HOME/conf/wrapper.conf
-echo "wrapper.java.additional.3=-Dsonar.enableStopCommand=true" >> $SQ_HOME/conf/wrapper.conf
-echo "wrapper.java.additional.4=-Dsonar.app.log.level=TRACE" >> $SQ_HOME/conf/wrapper.conf
+echo "" >> $SQ_HOME/conf/wrapper.conf
+echo "wrapper.java.additional.3=-Xdebug" >> $SQ_HOME/conf/wrapper.conf
+echo "wrapper.java.additional.4=-Xrunjdwp:transport=dt_socket,address=6006,server=y,suspend=y" >> $SQ_HOME/conf/wrapper.conf
+echo "wrapper.java.additional.5=-Dsonar.enableStopCommand=true" >> $SQ_HOME/conf/wrapper.conf
+echo "wrapper.java.additional.6=-Dsonar.log.app.level=TRACE" >> $SQ_HOME/conf/wrapper.conf
