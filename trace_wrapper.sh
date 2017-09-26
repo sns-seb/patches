0f1f79299e760a17ced4229177d9cb8c10f7679f
Sébastien Lesaint <sebastien.lesaint@sonarsource.com>
2017-09-26 11:16:13 +0200

    fix trace_wrapper

--- trace_wrapper.sh
+++ trace_wrapper.sh
@@ -7,7 +7,11 @@ set -euo pipefail
 
 SQ_HOME=$1
 
+source scripts/property_utils.sh
+
 echo "enabling TRACE logs in App"
 
 echo "" >> $SQ_HOME/conf/wrapper.conf
-echo "wrapper.java.additional.1=-Dsonar.app.log.level=TRACE" >> $SQ_HOME/conf/wrapper.conf
+echo "wrapper.java.additional.3=-Dsonar.log.app.level=TRACE" >> $SQ_HOME/conf/wrapper.conf
+
+set_property "sonar.log.level.app" "TRACE" $SQ_HOME/conf/sonar.properties
