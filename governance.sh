e501670ef7cb013d5596e7a26ea6791d9c769a67
Sébastien Lesaint <sebastien.lesaint@sonarsource.com>
2017-09-26 11:17:24 +0200

    fix governance.sh: setting license by sonar.properties does not work

--- governance.sh
+++ governance.sh
@@ -9,10 +9,8 @@ source scripts/property_utils.sh
 
 SQ_HOME=$1
 
-GOVERNANCE_FILE=~/DEV/governance/sonar-governance-plugin/target/sonar-governance-plugin-1.0-SNAPSHOT.jar
+GOVERNANCE_FILE=~/DEV/governance/sonar-governance-plugin/target/sonar-governance-plugin-2.0-SNAPSHOT.jar
 EXT_DIR=$SQ_HOME/extensions/plugins/
 echo "copy $GOVERNANCE_FILE to $EXT_DIR"
 cp  $GOVERNANCE_FILE $EXT_DIR
-
-#set_property sonar.governance.license.secured `cat scripts/patches/governance_license.txt` $SQ_HOME/conf/sonar.properties
 
