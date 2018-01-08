32d08ba8765cfb974e99b5f923050525a547cec7
Sébastien Lesaint <sebastien.lesaint@sonarsource.com>
2018-01-08 11:00:06 +0100

    upgrade branch to 1.1-SNAPSHOT

--- branch.sh
+++ branch.sh
@@ -9,7 +9,7 @@ source scripts/property_utils.sh
 
 SQ_HOME=$1
 
-BRANCH_FILE=~/DEV/branch/sonar-branch-plugin/target/sonar-branch-plugin-1.0-SNAPSHOT.jar
+BRANCH_FILE=~/DEV/branch/sonar-branch-plugin/target/sonar-branch-plugin-1.1-SNAPSHOT.jar
 EXT_DIR=$SQ_HOME/extensions/plugins/
 echo "copy $BRANCH_FILE to $EXT_DIR"
 cp  $BRANCH_FILE $EXT_DIR
