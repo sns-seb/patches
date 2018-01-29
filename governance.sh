59d51d79ab226dab45fdc54de8bdc0d8f97b4a67
Sébastien Lesaint <sebastien.lesaint@sonarsource.com>
2018-01-29 09:14:18 +0100

    governance.sh support core-plugins

--- governance.sh
+++ governance.sh
@@ -9,7 +9,7 @@ source scripts/property_utils.sh
 
 SQ_HOME=$1
 
-GOVERNANCE_FILE=~/DEV/governance/sonar-governance-plugin/target/sonar-governance-plugin-2.0-SNAPSHOT.jar
+GOVERNANCE_FILE=~/DEV/core-plugins/sonar-governance-plugin/target/sonar-governance-plugin-7.1-SNAPSHOT.jar
 EXT_DIR=$SQ_HOME/extensions/plugins/
 echo "copy $GOVERNANCE_FILE to $EXT_DIR"
 cp  $GOVERNANCE_FILE $EXT_DIR
